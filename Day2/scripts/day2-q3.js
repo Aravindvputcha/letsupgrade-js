e8b71e295cc4fa9cf4f0f2745a8750d6145f7b83
Aravindvputcha <49046735+Aravindvputcha@users.noreply.github.com>
2020-07-16 14:16:23 +0530

    Add files via upload

---
+++ Day2/scripts/day2-q3.js
@@ -0,0 +1,9 @@
+
+b= confirm("Age is 21+  ??")
+
+if  (b == true){
+    console.log("you can drink beer");
+}
+else{
+    console.log("you cant drink beer")
+}
---
+++ Day2/scripts/day2.js
@@ -0,0 +1,24 @@
+a= prompt("Enter the input");
+console.log(a);
+
+
+
+
+
+
+
+let s ="Aravind";  
+console.log(s.toUpperCase()); 
+console.log(s.toLowerCase());  
+console.log(s);
+console.log(s);   
+console.log(s.trim()); 
+console.log(s.startsWith("vi"));
+console.log(s.endsWith("n"));
+console.log(s.charAt(0)); 
+console.log(s.charAt(3));  
+console.log(s.length())
+
+
+
+
